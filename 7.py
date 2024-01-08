929598288ab280978bfebb006ff2550adde7f20c
Random Guy <94104897+random-1234@users.noreply.github.com>
2024-01-08 13:15:42 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,40 +1,34 @@
+def bankers_algorithm(allocation, max_claim, available):
+    n_processes = len(allocation)
+    n_resources = len(available)
 
-class BankersAlgorithm:
-    def __init__(self, allocation, max_claim, available):
-        self.allocation = allocation
-        self.max_claim = max_claim
-        self.available = available
-        self.n_processes = len(allocation)
-        self.n_resources = len(available)
-
-    def is_safe_state(self, process, work, finish):
-        for i in range(self.n_resources):
-            if self.max_claim[process][i] - self.allocation[process][i] > work[i]:
+    def is_safe_state(process, work, finish):
+        for i in range(n_resources):
+            if max_claim[process][i] - allocation[process][i] > work[i]:
                 return False
         return True
 
-    def execute(self):
-        work = self.available[:]
-        finish = [False] * self.n_processes
-        safe_sequence = []
+    work = available[:]
+    finish = [False] * n_processes
+    safe_sequence = []
 
-        while True:
-            found = False
-            for i in range(self.n_processes):
-                if not finish[i] and self.is_safe_state(i, work, finish):
-                    for j in range(self.n_resources):
-                        work[j] += self.allocation[i][j]
-                    finish[i] = True
-                    safe_sequence.append(i)
-                    found = True
+    while True:
+        found = False
+        for i in range(n_processes):
+            if not finish[i] and is_safe_state(i, work, finish):
+                for j in range(n_resources):
+                    work[j] += allocation[i][j]
+                finish[i] = True
+                safe_sequence.append(i)
+                found = True
 
-            if not found:
-                break
+        if not found:
+            break
 
-        if all(finish):
-            print("Safe Sequence:", safe_sequence)
-        else:
-            print("Unsafe state. System is in deadlock.")
+    if all(finish):
+        print("Safe Sequence:", safe_sequence)
+    else:
+        print("Unsafe state. System is in deadlock.")
 
 if __name__ == "__main__":
     allocation_matrix = [
@@ -55,5 +49,4 @@ if __name__ == "__main__":
 
     available_resources = [3, 3, 2]
 
-    banker = BankersAlgorithm(allocation_matrix, max_claim_matrix, available_resources)
-    banker.execute()
+    bankers_algorithm(allocation_matrix, max_claim_matrix, available_resources)
